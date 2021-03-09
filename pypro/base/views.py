219f001a60f48d1f135cb46afa8777351a00f744
elaine <elainevieira@hotmail.com>
2021-03-09 10:11:25 -0300

    Criação de Aba de Modulos #close30

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render
 # def home(request):
 # raise ValueError()
 # return HttpResponse('<html><body>Olá Django</body></html><', content_type='text/html')
+from pypro.modulos import facade
+
 
 def home(request):
-    return render(request, 'base/home.html',)
+    return render(request, 'base/home.html', {})
